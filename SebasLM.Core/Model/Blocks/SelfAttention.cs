40fb1bf0e85f9d6f5bb3fbd4bf7a9ce6ab1a7ac0
Mirrihk <sebas6721@gmail.com>
2025-10-02 18:01:17 -0400

    torch tensor method methed for forward added

--- SebasLM.Core/Model/Blocks/SelfAttention.cs
+++ SebasLM.Core/Model/Blocks/SelfAttention.cs
@@ -36,6 +36,47 @@ namespace SebasLM.Core.Model.Blocks
             RegisterComponents();
         }
 
+        public override TorchTensor forward(TorchTensor x, TorchTensor? mask = null)
+        {
+            var (B, T, D) = (x.shape[0], x.shape[1], x.shape[2]);
+
+            var q = qProj.forward(x);
+            var k = qProj.forward(x);
+            var v = qProj.forward(x);
+
+            // reshape to (B, nHeads, T, headDim)
+            q = q.view(B, T, nHeads, headDim).transpose(1, 2);
+            k = k.view(B, T, nHeads, headDim).transpose(1, 2);
+            v = v.view(B, T, nHeads, headDim).transpose(1, 2);
+
+            // score = (B, nHeads, T, T)
+            var scores = q.matmul(k.transpose(-2, -1)) * scale;
+
+            if (mask is null)
+            {
+                var casual = torch.tril(torch.ones(new long[] { T, T }, dtype: torch.int8, device: x.device));
+                var negInf = torch.tensor(double.NegativeInfinity, dtype: scores.dtype, device: x.device);
+                var where = casual.eq(0).unsqueeze(0).unsqueeze(0);
+                scores = scores.where(where, negInf);
+            }
+            else
+            {
+                scores = scores + mask;
+            }
+
+            var attn = functional.softmax(scores, -1);
+            attn = dropoutAttn.forward(attn);
+
+            var ctx = attn.matmul(v); // (B, nHeads, T, headDim)
+
+            // merge heads : [B, T, headDim] -> [B, T, dModel]
+            ctx = ctx.transpose(1, 2).contiguous().view(B, T, D);
+
+            var outProj = oProj.forward(ctx);
+            outProj = dropoutProj.forward(outProj);
+            return outProj;
+        }
+
 
     }
 
